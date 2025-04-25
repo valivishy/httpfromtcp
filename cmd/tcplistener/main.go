a480edf38184d41cb0e9d8fafc91526492c5464a
valentinvisan <valentin.visan.vv@gmail.com>
2025-04-25 20:54:43 +0300

    feature :: use FromReader

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/valivishy/httpfromtcp/internal/request"
 	"io"
 	"net"
-	"strings"
 )
 
 func main() {
@@ -20,54 +20,17 @@ func main() {
 			panic(err)
 		}
 		fmt.Println("Connection accepted")
-		channel := getLinesChannel(accept)
 
-		for value := range channel {
-			fmt.Println(value)
-		}
+		fromReader, err := request.FromReader(accept)
+		fmt.Println("Request line:")
+		line := fromReader.RequestLine
+		fmt.Printf("- Method: %s\n", line.Method)
+		fmt.Printf("- Target: %s\n", line.RequestTarget)
+		fmt.Printf("- Version: %s\n", line.HttpVersion)
+
 		fmt.Println("Connection closed")
 		safeClose(accept)
 	}
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	channel := make(chan string)
-
-	go func() {
-		bytes := make([]byte, 8)
-		storage := ""
-		for {
-			n, err := f.Read(bytes)
-			if n > 0 {
-				storage += string(bytes[:n])
-				if strings.Contains(storage, "\n") {
-					split := strings.Split(storage, "\n")
-					channel <- split[0]
-					storage = split[1]
-				}
-				if n < 8 {
-					if len(storage) > 0 {
-						channel <- storage
-					}
-
-					close(channel)
-					return
-				}
-			}
-			if err == io.EOF {
-				if len(storage) > 0 {
-					channel <- storage
-				}
-
-				close(channel)
-				return
-			}
-			if err != nil {
-				panic(err)
-			}
-		}
-	}()
-	return channel
 }
 
 func safeClose(closer io.Closer) {
